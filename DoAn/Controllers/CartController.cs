9105c8882c1a6e78948f35a44ef5305c6cf86e1d
Lê Minh Thanh <lmthanh203@gmail.com>
2022-11-04 21:49:04 +0700

    done

---
+++ DoAn/Controllers/CartController.cs
@@ -0,0 +1,74 @@
+﻿using DoAn.Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public List<CartItem> GetCart()
+        {
+            List<CartItem> myCart = Session["GioHang"] as
+           List<CartItem>;
+            //Nếu giỏ hàng chưa tồn tại thì tạo mới và đưa vào Session
+            if (myCart == null)
+            {
+                myCart = new List<CartItem>();
+                Session["GioHang"] = myCart;
+            }
+            return myCart;
+        }
+        public ActionResult AddToCart(int id)
+        {
+            //Lấy giỏ hàng hiện tại
+            List<CartItem> myCart = GetCart();
+            CartItem currentProduct = myCart.FirstOrDefault(p => p.ProductID == id);
+            if (currentProduct == null)
+            {
+                currentProduct = new CartItem(id);
+                myCart.Add(currentProduct);
+            }
+            else
+            {
+                currentProduct.Number++; //Sản phẩm đã có trong giỏ thì tăng số lượng lên 1
+            }
+            return RedirectToAction("Index", "CustomerProducts", new
+            {
+                id = id
+            });
+        }
+        private int GetTotalNumber()
+        {
+            int totalNumber = 0;
+            List<CartItem> myCart = GetCart();
+            if (myCart != null)
+                totalNumber = myCart.Sum(sp => sp.Number);
+            return totalNumber;
+        }
+        private decimal GetTotalPrice()
+        {
+            decimal totalPrice = 0;
+            List<CartItem> myCart = GetCart();
+            if (myCart != null)
+                totalPrice = myCart.Sum(sp => sp.FinalPrice());
+            return totalPrice;
+        }
+        public ActionResult GetCartInfo()
+        {
+            List<CartItem> myCart = GetCart();
+            //Nếu giỏ hàng trống thì trả về trang ban đầu
+            if (myCart == null || myCart.Count == 0)
+            {
+                return RedirectToAction("Index", "CustomerProducts");
+            }
+            ViewBag.TotalNumber = GetTotalNumber();
+            ViewBag.TotalPrice = GetTotalPrice();
+            return View(myCart); //Trả về View hiển thị thông tin giỏ hàng
+        }
+
+    }
+}
---
+++ DoAn/Models/EF/CartItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn.Models.EF
+{
+    public class CartItem
+    {
+        OnlineShopSM db = new OnlineShopSM();
+        public int ProductID { get; set; }
+        public string NamePro { get; set; }
+        public string ImagePro { get; set; }
+        public decimal Price { get; set; }
+        public int Number { get; set; }
+        //Tính FinalPrice = Price * Number
+        public decimal FinalPrice()
+        {
+            return Number * Price;
+        }
+        public CartItem(int ProductID)
+        {
+            this.ProductID = ProductID;
+
+            var productDB = db.Products.Single(s => s.ID == this.ProductID);
+            this.NamePro = productDB.Name;
+            this.ImagePro = productDB.Image;
+            this.Price = (decimal)productDB.Price;
+            this.Number = 1;
+        }
+
+    }
+}
